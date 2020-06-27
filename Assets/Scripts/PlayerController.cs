3141a602c8486d166496cf4a80a6a88b75f689f3
Justin Lim <justinlimweiyang@gmail.com>
2020-06-27 18:46:14 +0800

    Fixed hand rotations, did city scaling, sword script needs some work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     float verticalInput;
 
     public float moveSpeed = 5f;
+    //public Rigidbody rb;
     public CharacterController cc;
 
     // Start is called before the first frame update
@@ -21,14 +22,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("HorizontalK");
-        verticalInput = Input.GetAxis("VerticalK");
+        horizontalInput = Input.GetAxis("LeftTrackpadHorizontal");
+        verticalInput = Input.GetAxis("LeftTrackpadVertical");
     }
 
     void FixedUpdate()
     {
         Quaternion rot = Quaternion.Euler(0, cam.transform.rotation.eulerAngles.y, 0);
         Vector3 move = rot * new Vector3(horizontalInput, 0, verticalInput);
+        //transform.Translate(move * moveSpeed * Time.deltaTime);
+        //rb.AddForce(move * moveSpeed * Time.deltaTime, ForceMode.Impulse);
         cc.Move(move * moveSpeed * Time.deltaTime);
     }
 }
